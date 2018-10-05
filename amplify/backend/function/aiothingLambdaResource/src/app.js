4c4a4a21a5865f70d3b9d988e47924e169de5b14
hansontech <denniskung68@hotmail.com>
2018-10-05 11:02:51 +1000

    solution part

---
+++ amplify/backend/function/aiothingLambdaResource/src/app.js
@@ -0,0 +1,103 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+
+/**********************
+ * Example get method *
+ **********************/
+
+app.get('/number', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
+
+app.get('/number/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.post('/number', function(req, res) {
+  // Add your code here
+  const correct = 12;
+  let guess = req.body.guess
+  let result = "xxxxx"
+
+  console.log("POST man: ", req.apiGateway.event)
+  
+  if (guess === correct) {
+    result = "correct";
+  } else if (guess > correct) {
+    result = "high";
+  } else if (guess < correct) {
+    result = "low";
+  }
+
+  res.json({ result })
+});
+
+app.post('/number/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.put('/number', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+app.put('/number/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example delete method *
+****************************/
+
+app.delete('/number', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.delete('/number/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
